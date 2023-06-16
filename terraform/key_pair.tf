2770b35906f5b0a303f7f8139bde8d41e3c76567
Narendra <tirumaninarendrababu@gmail.com>
2023-06-17 00:09:05 +0530

    pem file permissions updated

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -11,4 +11,5 @@ resource "aws_key_pair" "key_pair" {
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
   filename = "/home/ubuntu/.ssh/${var.key_name}"
+  file_permission = "400"
 }
